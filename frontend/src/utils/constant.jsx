977205bf461a59094088fd8bd2c2c1d8dfb3fc61
aditiwari07 <adityatiwar07@gmail.com>
2024-09-16 23:37:09 +0530

    changes

--- frontend/src/utils/constant.jsx
+++ frontend/src/utils/constant.jsx
@@ -1,7 +1,7 @@
-const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+const USER_API_END_POINT="https://job-portal-project-9k47.onrender.com/api/v1/user";
 export default USER_API_END_POINT;
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job"
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application"
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company"
+export const JOB_API_END_POINT = "https://job-portal-project-9k47.onrender.com/api/v1/job"
+export const APPLICATION_API_END_POINT = "https://job-portal-project-9k47.onrender.com/api/v1/application"
+export const COMPANY_API_END_POINT = "https://job-portal-project-9k47.onrender.com/api/v1/company"
 
 
