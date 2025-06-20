d8d0ecacb7c1ee6fd2d4c31465ae32a6e6f56cee
aditiwari07 <adityatiwar07@gmail.com>
2025-06-20 18:07:18 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-portal-project-w5hc.onrender.com',
     credentials:true
 }
 
